92f48cdd13caa4ba3ff242003667f43daab69331
Anand <anandkshuklap@gmail.com>
2020-12-25 22:07:31 +0530

    hello world

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App(props) {
+  return (
+    <div className="">
+      <header className="">       
+        <h1>Hello World</h1>
+        <p>Welcome to my website</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
